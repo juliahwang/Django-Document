f527cea946fa039fc30ab52fabefe79215a79d8a
juliahwang <qufskan9396@gmail.com>
2017-06-01 20:59:16 +0900

    make detail template

--- djangopolls/polls/views.py
+++ djangopolls/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from polls.models import Question
 
@@ -23,10 +23,7 @@ def detail(request, question_id):
         :param question_id: 질문의 pk번호
         :return: pk번호에 해당하는 질문의 투표페이지 
         """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
